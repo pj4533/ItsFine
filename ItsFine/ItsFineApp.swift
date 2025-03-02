99781019b32976f1790f861c9410c8d86d0053c3
PJ Gray <pj@evertrue.com>
2025-03-02 15:38:41 -0500

    Added initial xcode proj

---
+++ ItsFine/ItsFineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItsFineApp.swift
+//  ItsFine
+//
+//  Created by PJ Gray on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ItsFineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ItsFineTests/ItsFineTests.swift
@@ -0,0 +1,17 @@
+//
+//  ItsFineTests.swift
+//  ItsFineTests
+//
+//  Created by PJ Gray on 3/2/25.
+//
+
+import Testing
+@testable import ItsFine
+
+struct ItsFineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
